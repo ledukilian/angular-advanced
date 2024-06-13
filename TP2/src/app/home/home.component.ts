259d449c15c6112b90bd3afba297f9fef6f56b52
Kilian LE DU <ledu.kilian@gmail.com>
2024-06-13 16:16:55 +0200

    Ajout modale + comportement

---
+++ TP2/src/app/home/home.component.ts
@@ -0,0 +1,40 @@
+import {Component, inject, signal} from '@angular/core';
+import { Checklist } from '../shared/interfaces/checklist';
+import { ModalComponent } from '../shared/ui/modal/modal.component';
+import { FormModalComponent } from "../shared/ui/form-modal/form-modal.component";
+import { FormBuilder } from "@angular/forms";
+
+@Component({
+  standalone: true,
+  selector: 'app-home',
+  template: `
+    <header>
+      <h1>Quicklists</h1>
+      <button (click)="checklistBeingEdited.set({})">Add Checklist</button>
+    </header>
+
+    <app-modal [isOpen]="!!checklistBeingEdited()">
+      <ng-template>
+        <app-form-modal
+          [title]="
+            checklistBeingEdited()?.title
+              ? checklistBeingEdited()!.title!
+              : 'Add Checklist'
+          "
+          [formGroup]="checklistForm"
+          (close)="checklistBeingEdited.set(null)"
+        />
+      </ng-template>
+    </app-modal>
+  `,
+  imports: [ModalComponent, FormModalComponent],
+})
+export default class HomeComponent {
+  formBuilder = inject(FormBuilder);
+
+  checklistBeingEdited = signal<Partial<Checklist> | null>(null);
+
+  checklistForm = this.formBuilder.nonNullable.group({
+    title: [''],
+  });
+}
---
+++ TP2/src/app/shared/ui/form-modal/form-modal.component.ts
@@ -0,0 +1,36 @@
+import { KeyValuePipe } from '@angular/common';
+import { Component, input, output } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  standalone: true,
+  selector: 'app-form-modal',
+  template: `
+<header>
+  <h2>{{ title() }}</h2>
+  <button (click)="close.emit()">close</button>
+</header>
+<section>
+  <form [formGroup]="formGroup()" (ngSubmit)="save.emit(); close.emit()">
+          @for (control of formGroup().controls | keyvalue; track control.key){
+  <div>
+  <label [for]="control.key">{{ control.key }}</label>
+  <input
+                [id]="control.key"
+                type="text"
+                [formControlName]="control.key"
+              />
+  </div>
+          }
+  <button type="submit">Save</button>
+  </form>
+</section>
+  `,
+  imports: [ReactiveFormsModule, KeyValuePipe],
+})
+export class FormModalComponent {
+  formGroup = input.required<FormGroup>();
+  title = input.required<string>();
+  save = output();
+  close = output();
+}
